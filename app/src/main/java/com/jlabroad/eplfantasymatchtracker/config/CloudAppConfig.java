c33adcc2e29911ee64d3adcb08b4957b6770b26a
Jonathan LaBroad <jon.labroad@gmail.com>
2017-10-31 14:50:51 -0400

    Trying to build in codebuild

--- app/src/main/java/com/jlabroad/eplfantasymatchtracker/config/CloudAppConfig.java
+++ app/src/main/java/com/jlabroad/eplfantasymatchtracker/config/CloudAppConfig.java
@@ -1,5 +1,10 @@
 package com.jlabroad.eplfantasymatchtracker.config;
 
+import com.jlabroad.eplfantasymatchtracker.data.TeamIdName;
+
+import java.util.HashMap;
+
 public class CloudAppConfig {
     public int CurrentGameWeek;
+    public HashMap<Integer, TeamIdName> AvailableTeams = new HashMap<>();
 }
---
+++ app/src/main/java/com/jlabroad/eplfantasymatchtracker/data/TeamIdName.java
@@ -0,0 +1,7 @@
+package com.jlabroad.eplfantasymatchtracker.data;
+
+public class TeamIdName {
+    public int teamId;
+    public String teamName;
+    public String teamOwner;
+}

971c91571829fb709a295f8cc406ad09d72bdc49
Jonathan LaBroad <jon.labroad@gmail.com>
2017-10-31 17:24:38 -0400

    Team choice if not team is subscribed

--- app/src/main/java/com/jlabroad/eplfantasymatchtracker/ChooseTeamActivity.java
+++ app/src/main/java/com/jlabroad/eplfantasymatchtracker/ChooseTeamActivity.java
@@ -1,35 +1,119 @@
 package com.jlabroad.eplfantasymatchtracker;
 
+import android.app.Activity;
+import android.app.ListActivity;
+import android.app.LoaderManager;
+import android.content.Context;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.graphics.Color;
+import android.provider.ContactsContract;
 import android.provider.Settings;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
+import com.jlabroad.eplfantasymatchtracker.config.CloudAppConfig;
 import com.jlabroad.eplfantasymatchtracker.config.DeviceConfigurator;
 import com.jlabroad.eplfantasymatchtracker.config.GlobalConfig;
+import com.jlabroad.eplfantasymatchtracker.data.TeamIdName;
 
-public class ChooseTeamActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class ChooseTeamActivity extends Activity {
+
+    private ArrayList<TeamIdName> _teams = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_team);
 
-        // Get the Intent that started this activity and extract the string
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(MatchView.TEAM_CHOOSE_REQUEST);
+        final ListView listview = (ListView) findViewById(R.id.team_list_view);
 
-        // JUST FOR TESTING THE FLOW
-        //GlobalConfig.deviceConfig.addSubscription(2365803, "The Vardy Boys");
-        //new DeviceConfigurator().writeConfig(GlobalConfig.deviceConfig, getUniqueDeviceId());
+        for (TeamIdName teamIdName : GlobalConfig.cloudAppConfig.AvailableTeams.values()) {
+            _teams.add(teamIdName);
+        }
 
-        GlobalConfig.deviceConfig.addSubscription(1326527, "Pinky and De Bruyne");
+        final StableArrayAdapter adapter = new StableArrayAdapter(this,
+                android.R.layout.simple_list_item_1, _teams);
+        listview.setAdapter(adapter);
 
-        Intent backToMain = new Intent(getApplicationContext(), MatchView.class);
-        startActivity(backToMain);
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+                final TeamIdName item = (TeamIdName) parent.getItemAtPosition(position);
+                view.animate().setDuration(500).alpha(0)
+                        .withEndAction(new Runnable() {
+                            @Override
+                            public void run() {
+                                GlobalConfig.deviceConfig.addSubscription(item.teamId, item.teamName);
+                                new DeviceConfigurator().writeConfig(GlobalConfig.deviceConfig, getUniqueDeviceId());
+
+                                // Go to main activity
+                                //Intent intent = new Intent(getApplicationContext(), MatchView.class);
+                                //startActivity(intent);
+                                finish();
+                            }
+                        });
+            }
+        });
     }
 
-    private  String getUniqueDeviceId() {
+    private String getUniqueDeviceId() {
         return Settings.Secure.getString(getApplicationContext().getContentResolver(), Settings.Secure.ANDROID_ID);
+    }
+
+    private class StableArrayAdapter extends ArrayAdapter<TeamIdName> {
+
+        public StableArrayAdapter(Context context, int textViewResourceId,
+                                  ArrayList<TeamIdName> objects) {
+            super(context, textViewResourceId, objects);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            TeamIdName item = getItem(position);
+            return item.teamId;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            String teamText = String.format("%s - %s",_teams.get(position).teamName, _teams.get(position).teamOwner);
+            TextView newView = null;
+            if (convertView == null) {
+                newView = new TextView(getApplicationContext());
+            }
+            else {
+                newView = (TextView) convertView;
+            }
+            newView.setText(teamText);
+            newView.setTextColor(Color.BLACK);
+            newView.setTextSize(22.0f);
+            newView.setPadding(0, 20, 0, 20);
+            return newView;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
     }
 }

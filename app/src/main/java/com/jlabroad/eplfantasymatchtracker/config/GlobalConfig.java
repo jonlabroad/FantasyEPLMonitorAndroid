e252e9454d35118d46d56a6dd179fd26472a8456
Jonathan LaBroad <jon.labroad@gmail.com>
2018-08-12 08:07:15 -0400

    typo

--- app/src/main/java/com/jlabroad/eplfantasymatchtracker/config/GlobalConfig.java
+++ app/src/main/java/com/jlabroad/eplfantasymatchtracker/config/GlobalConfig.java
@@ -4,7 +4,7 @@ public class GlobalConfig {
     public static String S3Bucket = "fantasyeplmatchtracker";
     public static String PlatformApplicationId = "arn:aws:sns:us-east-1:796987500533:app/GCM/EPL_Fantasy_MatchTracker";
 
-    public static String MatchInfoRootFmt = "data/Season2108/%d/%d/%d/MatchInfo";
+    public static String MatchInfoRootFmt = "data/Season2018/%d/%d/%d/MatchInfo";
 
     public static DeviceConfig deviceConfig;
     public static CloudAppConfig cloudAppConfig;
